0e1d806cd8c55677c773bf55c7a5eeee6707037c
prashanttnegi <prashant.negi0407@gmail.com>
2025-04-11 00:22:21 +0530

    Database Updated

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,7 +1,7 @@
 // routes/authRoutes.js
 const express = require('express');
 const router = express.Router();
-const authController = require('./controllers/authController');
+const authController = require('../controllers/authController');
 
 router.post('/register', authController.register);
 router.post('/login', authController.login);
